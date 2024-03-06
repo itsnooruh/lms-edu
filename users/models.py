bb88d170f8bc299ca877663492af774d4eeb0649
itsnoorah <mokhinurkhasanova365@gmail.com>
2024-03-07 00:52:46 +0500

    feat: add users app (models, login/logout)

---
+++ users/models.py
@@ -0,0 +1,17 @@
+from django.contrib.auth import models as auth
+from django.db import models
+
+
+class User(auth.AbstractUser):
+    USER_ROLE_CHOICES = ((1, "Administrator"), (2, "Student"), (3, "Parent"))
+
+    role = models.PositiveIntegerField(choices=USER_ROLE_CHOICES, default=2)
+    phone_number = models.CharField(max_length=15, unique=True, db_index=True)
+    parent = models.ForeignKey(
+        "self", on_delete=models.CASCADE, null=True, blank=True
+    )
+
+    REQUIRED_FIELDS = ["phone_number"]
+
+    def __str__(self):
+        return f"({self.role}) {self.first_name} {self.last_name}"
